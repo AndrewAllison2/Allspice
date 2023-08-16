83c37c478fc7b150fa077c0ae013ebc08f7f28ca
Andrew Allison <andrewallison5211@gmail.com>
2023-08-16 14:39:53 -0600

    env setup

---
+++ Allspice.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'dev-74troqf32atk1sf4.us.auth0.com'
+export const clientId = '6y8FgqSRtZwVSJvNDZe6GvBpGGekZsTM'
+export const audience = 'classroom.com'
