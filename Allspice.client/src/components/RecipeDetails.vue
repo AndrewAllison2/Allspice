9d42a4efc7620bd602597d9f44d42b9acbfc6d91
Andrew Allison <andrewallison5211@gmail.com>
2023-08-18 10:17:53 -0600

    modal stuff

--- Allspice.client/src/components/RecipeDetails.vue
+++ Allspice.client/src/components/RecipeDetails.vue
@@ -2,12 +2,33 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-5">
-        <img src="" alt="">
+        <img class="img-fluid recipe-img" :src="activeRecipe?.imgUrl" :alt="activeRecipe?.title">
       </div>
 
       <div class="col-7">
-        Recipe
-        <!-- <h1>{{ activeRecipe.title }}</h1> -->
+        <div class="d-flex align-items-center justify-content-between mb-4">
+          <h1>{{ activeRecipe?.title }}</h1>
+          <p class="category-elem text-white">{{ activeRecipe?.category }}</p>
+        </div>
+
+        <div class="row justify-content-around">
+          <div class="col-5 card p-0">
+            <div class="text-center text-white card-background">
+              <h4 class="mt-1">Recipe Steps</h4>
+            </div>
+              <p>{{ activeRecipe?.instructions }}</p>
+            <div>
+
+            </div>
+          </div>
+
+
+          <div class="col-5 card p-0">
+            <div class="text-center text-white card-background">
+              <h4 class="mt-1">Ingredients</h4>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -34,5 +55,20 @@ export default {
 
 
 <style lang="scss" scoped>
+
+.recipe-img{
+  object-fit: cover;
+}
+
+.category-elem{
+  background-color: gray;
+  border-radius: 2px;
+  padding: .5em;
+}
+
+.card-background{
+  background-color: #527360;
+;
+}
 
 </style>
